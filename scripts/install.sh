b7fd11301d73f7e35de14e3fd08ca2488277aa61
Leucir Marin <lmarinj@us.ibm.com>
2017-06-15 16:37:22 -0500

    fixing curl download wsk

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,5 +2,8 @@
 #https://openwhisk.ng.bluemix.net/cli/go/download/linux/amd64/OpenWhisk_CLI-linux.tgz
 #https://openwhisk.ng.bluemix.net/cli/go/download/linux/amd64/wsk
 
+#create temp folder
+mkdir ~/wsk_temp
+
 #install openwhisk CLI
-curl -o wsk -L https://openwhisk.ng.bluemix.net/cli/go/download/linux/amd64/wsk
+curl -o ~/wsk_temp/wsk -L https://openwhisk.ng.bluemix.net/cli/go/download/linux/amd64/wsk
